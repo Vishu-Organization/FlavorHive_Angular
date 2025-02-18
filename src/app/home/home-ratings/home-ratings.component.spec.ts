2f4723c888cf85cb351a21b9c1bdcad7cc2a60ee
Vishu Kumar <1992vishukumar@gmail.com>
2025-02-18 11:19:26 +0530

    24: ratings section design

---
+++ src/app/home/home-ratings/home-ratings.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HomeRatingsComponent } from './home-ratings.component';
+
+describe('HomeRatingsComponent', () => {
+  let component: HomeRatingsComponent;
+  let fixture: ComponentFixture<HomeRatingsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HomeRatingsComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(HomeRatingsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/home/home-ratings/home-ratings.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-ratings',
+  templateUrl: './home-ratings.component.html',
+  styleUrls: ['./home-ratings.component.scss']
+})
+export class HomeRatingsComponent {
+
+}
