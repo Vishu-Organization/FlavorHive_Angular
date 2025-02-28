b35ab1a5ddaaf5c191af60038da1e8159105ffc3
Vishu Kumar <1992vishukumar@gmail.com>
2025-02-28 11:14:09 +0530

    upgrade to v16

--- src/resolvers/login/login.resolver.ts
+++ src/resolvers/login/login.resolver.ts
@@ -1,18 +1,13 @@
 import { Injectable } from '@angular/core';
-import {
-  Resolve,
-  RouterStateSnapshot,
-  ActivatedRouteSnapshot,
-} from '@angular/router';
+import { RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { SignupDataState } from 'src/store/auth/_interfaces';
 import { AuthDataActions } from 'src/store/auth/actions';
 
-
 @Injectable({
   providedIn: 'root',
 })
-export class LoginResolver implements Resolve<void> {
+export class LoginResolver {
   constructor(private store: Store<SignupDataState>) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): void {
