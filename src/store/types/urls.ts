9c3026f99406a8bf3782f5b38cc484e19885d7e8
Vishu Kumar <1992vishukumar@gmail.com>
2025-03-02 10:55:51 +0530

    43: router implementation added

--- src/store/types/urls.ts
+++ src/store/types/urls.ts
@@ -1,3 +1,4 @@
+import { routerReducer } from '@ngrx/router-store';
 import { authReducer, signupDataReducer } from '../auth/reducer';
 import { footerReducer } from '../footer/reducer';
 import { homeReducer } from '../home/reducer';
@@ -5,10 +6,11 @@ import { sharedReducer } from '../shared/reducer';
 
 export const VITE_SUPABASE_URL = 'https://opmliiavikfniknrvlgt.supabase.co';
 
-export const storeConfig = {
+export const reducers = {
   auth: authReducer,
   signupData: signupDataReducer,
   home: homeReducer,
   footer: footerReducer,
   shared: sharedReducer,
+  router: routerReducer,
 };
