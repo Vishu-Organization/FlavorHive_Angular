e87609cc92ac6fb78bf8593549db9e6d396874d6
Vishu Kumar <1992vishukumar@gmail.com>
2025-03-13 10:25:09 +0530

    53: dummy blog screen design

--- src/app/blog/blog/blog.component.ts
+++ src/app/blog/blog/blog.component.ts
@@ -1,13 +1,50 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Store } from '@ngrx/store';
+import { SharedService } from 'src/services/shared/shared.service';
+import { BlogRecipe, SharedState } from 'src/store/shared/_interfaces';
+import {
+  filter,
+  map,
+  Observable,
+  range,
+  repeat,
+  switchMap,
+  toArray,
+} from 'rxjs';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.scss']
+  styleUrls: ['./blog.component.scss'],
 })
 export class BlogComponent {
+  blogs$: Observable<BlogRecipe[] | null>;
+  blogLoading$: Observable<boolean>;
+  blogError$: Observable<string | null>;
 
+  constructor(
+    private sharedService: SharedService,
+    private store: Store<SharedState>
+  ) {
+    this.blogs$ = this.sharedService.blog$.pipe(
+      filter((blog): blog is BlogRecipe => !!blog),
+      switchMap(
+        (blog) =>
+          range(1, 20).pipe(
+            map(() => blog as BlogRecipe),
+            toArray()
+          ) // Repeat blog 20 times
+      )
+    );
+    this.blogLoading$ = this.sharedService.blogLoading$;
+    this.blogError$ = this.sharedService.blogError$;
+  }
+
+  trackByIndex(index: number) {
+    return index;
+  }
 }
